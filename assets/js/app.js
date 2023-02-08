8ddcab455b4cb18c6bd46d587fa4121408c5c9a2
CailyPhuc <phuc_web@caily.com.vn>
2023-02-08 11:10:43 +0700

    Add section about.

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,3 +22,14 @@ const typeWrite = () => {
 	}
 }
 typeWrite();
+
+// Open/close modal
+const modalBtn = document.querySelector('.js-modal-trigger');
+const modal = document.querySelector('.js-modal');
+const modalClose = document.querySelector('.js-modal-close');
+modalBtn.addEventListener('click', () => {
+	modal.style.display = 'flex';
+});
+modalClose.addEventListener('click', () => {
+	modal.style.display = 'none';
+});
